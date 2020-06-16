db73afc93f4e1415509e228be286c62dd1d80753
= <=>
2020-06-16 16:30:42 +0200

    init practica 2

---
+++ 201920_P2SI/Plantilla/src/pkg1920_p2si/Main.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1920_p2si;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fidel
+ */
+public class Main {
+    
+    
+    public static int Byte2Unsigned(byte b) {
+        return b & 0xFF;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Cargador MNIST de SI
+        MNISTLoader ml = new MNISTLoader();
+        ml.loadDBFromPath("./mnist_1000");
+        
+        //Accedo a las imagenes de dígito 1
+        ArrayList d0imgs = ml.getImageDatabaseForDigit(1);
+        
+        //Y cojo la tercera imagen del dígito 1
+        Imagen img = (Imagen) d0imgs.get(2);
+        
+        //La invierto para ilustrar como acceder a los pixels
+        byte imageData[] = img.getImageData();
+        for (int i = 0; i < imageData.length; i++){
+            
+            imageData[i] = (byte) (255 - imageData[i]);
+            System.out.print(Byte2Unsigned(imageData[i])+ ",");
+        }
+        
+        //Muestro la imagen invertida
+        MostrarImagen imgShow = new MostrarImagen();
+        imgShow.setImage(img);
+        imgShow.mostrar();
+
+        
+    }
+    
+}
