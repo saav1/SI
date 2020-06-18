e837f2f4bb7a7dedd60a37a9dca08b7a9e91bd3f
= <=>
2020-06-18 16:33:24 +0200

    s

--- 201920_P2SI/Plantilla/src/pkg1920_p2si/Adaboost.java
+++ 201920_P2SI/Plantilla/src/pkg1920_p2si/Adaboost.java
@@ -5,6 +5,7 @@
  */
 package pkg1920_p2si;
 import java.util.*;
+import java.io.*;
 /**
  *
  * @author stalynalejandro
@@ -14,32 +15,132 @@ public class Adaboost {
     private int porcentaje;
     private int debiles;
     private int pruebas;
+        
+    private ArrayList test          = new ArrayList();    
+    private ArrayList entrenamiento = new ArrayList();
+    private ArrayList correcto      = new ArrayList();
+    
+    
+    public MNISTLoader ml = new MNISTLoader();
+
     
     Adaboost(int porcentaje, int debiles, int pruebas){
         
         this.porcentaje = porcentaje;
         this.debiles    = debiles;
         this.pruebas    = pruebas;
+        this.ml.loadDBFromPath("./mnist_1000");
+        
+        clasificarImagenes();
+        clasificarCorrecto();
     }
+    
+    
+    private void clasificarImagenes(){
+        
+        for( int i = 0; i <= 9 ; i++ ){
+        
+            ArrayList imagenes = this.ml.getImageDatabaseForDigit(i);
+            
+            int numImagenesPorDigito = imagenes.size();
+            
+            System.out.println(" - digitos: " + numImagenesPorDigito);
+            
+            
+            for( int j = 0; j < numImagenesPorDigito; j++ ){
+                
+                Imagen img = (Imagen)imagenes.get(j);
+                
+                if(j < ((this.porcentaje * numImagenesPorDigito)/100)) entrenamiento.add(img);
+                else{
+                    test.add(img);
+                    correcto.add(i);
+                }
+                
+            }
+            
+            System.out.println("entrenamiento : " + entrenamiento.size());
+            System.out.println("test          : " + test.size() );
+            
+            
+        }
+        
+    }
+    
+    
+    private void clasificarCorrecto(){
+        
+        
+        for( int i = 0 ; i <= 9; i++ ){
+            
+            for( int j = 0; j < entrenamiento.size(); i++ ){
+            
+
+            }
+            
+            
+        }
+        
+    }
+    
+    
+
+    
+
+    
+    
+    
+    public ArrayList<Integer> aplicarClasificadorFuerte( ArrayList test, ArrayList<ClasificadorFuerte> fuertes  ){
+        ArrayList<Integer> h = new ArrayList();
+        return h;
+    }
+    
+    
+    
+    
+    public ClasificadorFuerte aplicarAdaboost( ArrayList<Imagen> entrenamiento, ArrayList correctos){
+        
+        ClasificadorFuerte fuerte = new ClasificadorFuerte();
+        
+        //Aquí hay que implementar el pseudocódigo de ADABOOST
+    
+        return fuerte;
+        
+    }
+    
+    
+    public int cuentaAciertos( ArrayList test, ArrayList correcto, ArrayList<Integer> mejoresH ){
+        return 0;
+    }
+    
     
     public ArrayList<ClasificadorFuerte> getClasificadoresFuertes(){
         
         ArrayList<ClasificadorFuerte> fuertes = new ArrayList();
         
-        for( int i = 0; i <= 9; ++i ){
+        for( int i = 0; i <= 9; i++ ){
            
-            fuertes.add( aplicarAdaboost(getAprendizaje(), getCorrectos().get(i)) );
+            fuertes.add( aplicarAdaboost(getEntrenamiento(), (ArrayList) getCorrecto().get(i)) );
             
         }
         
-    
+        return fuertes;
     }
     
-    public ClasificadorFuerte aplicarAdaboost( ArrayList<Imagen> entrenamiento, ArrayList<Boolean> correctos){
-        
-        //Aquí hay que implementar el pseudocódigo de ADABOOST
     
+    
+    public ArrayList getEntrenamiento(){
+        return this.entrenamiento;
     }
+    
+    public ArrayList getCorrecto(){
+        return this.correcto;
+    }
+    
+    public ArrayList getTest(){
+        return this.test;
+    }
+    
     
     
 }
--- 201920_P2SI/Plantilla/src/pkg1920_p2si/Main.java
+++ 201920_P2SI/Plantilla/src/pkg1920_p2si/Main.java
@@ -17,6 +17,19 @@ public class Main {
     public static int Byte2Unsigned(byte b) {
         return b & 0xFF;
     }
+    
+    public static void guardarClasificador(ArrayList<ClasificadorFuerte> fuerte, String file){
+        //Guardar el clasificador en un .txt
+        
+    }
+    
+    
+    public static ArrayList<ClasificadorFuerte> leerClasificadorFuerte(String file){
+        ArrayList<ClasificadorFuerte> clasFuerte = new ArrayList();
+        return clasFuerte;
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -56,24 +69,73 @@ public class Main {
         int numDebiles  = 100;
         int numAciertos = 0;
         
-        long start, end;
+        long init, fin;
 
         
-        ArrayList<Imagen>               imagenCargada;
+        ArrayList<Imagen>               imagenes;
         ArrayList<ClasificadorFuerte>   ClasificadoresFuertes;
         ArrayList<Integer>              mejoresH;
         
-        
+  
         Adaboost adaboost = new Adaboost(porcentaje, numDebiles, numPruebas);
+        
         
         if( args[0].equals("-t") ){
         
-            System.out.println("Entrenando para todos los dígitos\n");
+                    System.out.println("Entrenando para todos los dígitos\n");
+
             
-            start = System.currentTimeMillis();
+            init = System.currentTimeMillis();
             
-            ClasificadoresFuertes = 
+            ClasificadoresFuertes = adaboost.getClasificadoresFuertes();
             
+            guardarClasificador(ClasificadoresFuertes, "s");
+            
+            mejoressH = adaboost.aplicarClasificadorFuerte( adaboost.getTest(), ClasificadoresFuertes);
+            
+            numAciertos = adaboost.cuentaAciertos( adaboost.getTest(), adaboost.getCorrecto(), mejoresH);
+            
+            fin = System.currentTimeMillis();
+
+            
+                    System.out.println("\nFuerte Guardado en " + "" );
+                    System.out.println("\nreal vs fuerte");
+                    System.out.println(adaboost.getCorrecto() );
+                    System.out.println(mejoresH);
+                    System.out.println("\n** Resultados imagenes de testeo: ");
+                    System.out.println( "Aciertos:  " + numAciertos );
+                    System.out.println( "Fallos:    " + (adaboost.getTest().size() - numAciertos) );
+                    System.out.println("Porcentaje: " + (numAciertos*100/mejoresH.size()) +"%");
+                    System.out.println("\nLa tarea ha requerido "+ ( init - fin ) +" ms");
+
+            
+                
+        }else{
+            
+                    System.out.println("Cargar Clasificador fuerte y ejecutar sobre una imagen.\n");
+                    System.out.println("Clasificador Fuerte : " + " .txt");
+                    System.out.println("Imagen " + " ");
+            
+            
+            int digitoEvaluar = 1;
+            
+            ClasificadoresFuertes = leerClasificadorFuerte(".txt");
+            
+            imagenes = adaboost.ml.getImageDatabaseForDigit(digitoEvaluar);
+            
+            mejoresH = adaboost.aplicarClasificadorFuerte(imagenes, ClasificadoresFuertes );
+        
+            //Calcular número de aciertos.
+
+                    System.out.println("\n**generada vs real ");
+                    System.out.println(mejoresH);
+                    System.out.println(imagenes);
+
+                    System.out.println("\n** Resultados **");
+                    System.out.println( "Aciertos:  " + numAciertos );
+                    System.out.println( "Fallos:    " + (imagenes.size() - numAciertos) );
+                    System.out.println("Porcentaje: " + (numAciertos*100/mejoresH.size()) +"%");
+
             
         }
         
